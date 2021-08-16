a21b3fbdc81c6214c4a8d5e5d6dbcf57dde3f7f6
anugrah3199 <anugrahdixit93@gmail.com>
2021-08-16 15:06:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const text = document.querySelector("textarea");
+const translateBtn = document.querySelector("button");
+const result = document.querySelector(".result");
+const errordiv = document.querySelector(".error")
+var url = "https://api.funtranslations.com/translate/dothraki.json"
+
+function translate() {
+    const newurl = url + "?" + "text=" + text.value;
+    fetch(newurl).then(response => response.json())
+        .then(data => {
+            result.innerHTML = data.contents.translated;
+            result.style.display = "block";
+        })
+        .catch(error => {
+            errordiv.style.display = "block";
+            setTimeout(() => { errordiv.style.display = "none" }, 2000)
+        });
+}
+translateBtn.addEventListener("click", translate);
